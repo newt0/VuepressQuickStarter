39fd897fc1ea9124c20888cf35aff8a220bc8e74
newt0 <py_lambda@yahoo.co.jp>
2020-05-29 02:48:10 +0900

    globalPaginationを有効化。及び、一ページ毎の記事の表示数を10に設定

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -149,9 +149,9 @@ module.exports = {
      * Ref: https://vuepress-theme-blog.ulivz.com/config/#globalpagination
      */
 
-    // globalPagination: {
-    //   lengthPerPage: 10,
-    // },
+    globalPagination: {
+      lengthPerPage: 10,
+    },
 
     /**
      * Ref: https://vuepress-theme-blog.ulivz.com/config/#sitemap
